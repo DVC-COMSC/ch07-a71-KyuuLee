027985f9d713c72791174b1d08266d70c58718d0
Kyuu <69904775+KyuuLee@users.noreply.github.com>
2022-10-01 01:32:51 -0700

    Update main.py

--- main.py
+++ main.py
@@ -3,3 +3,12 @@
 # ******************************
 
 numbers = list(map(int, input().split()))
+
+total = sum(numbers)
+avg = total / len(numbers)
+
+for i in range(len(numbers)):
+	dist = abs(numbers[i]-avg)
+	print (f'{dist:.2f}', end=' ')
+
+print ()
